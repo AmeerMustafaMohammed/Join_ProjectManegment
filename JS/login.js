9a02500f2a6cde0b9cc1a9fafd801f68354a3841
Ameer Mohammed <ameer.coding1994@gmail.com>
2022-04-27 06:03:53 +0200

    second try

---
+++ JS/login.js
@@ -0,0 +1,11 @@
+console.log("it Works")
+
+
+
+function hideDiv(){
+
+   
+   let groupInput = document.getElementById("group-input")
+   console.log(groupInput)
+    groupInput.classList.add("display-none")
+}
