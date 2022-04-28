7e64d07a186e8dfa739d90cb808eae60cd137fed
Ameer Mohammed <ameer.coding1994@gmail.com>
2022-04-28 03:30:08 +0200

    login

--- JS/login.js
+++ JS/login.js
@@ -1,11 +1,49 @@
-console.log("it Works")
+// Register a new Group
+let allGroups = []
+
+if (loadArrayFromLS('allGroups')) {
+    allGroups = loadArrayFromLS('allGroups');
+}
+
+
+function rigisterGroup() {
+    let newGroupName = document.getElementById('new-group-input').value;
+    allGroups.push(newGroupName);
+    saveArrayInLS(allGroups);
+
+}
+
+
+function login(Demo) {
+    let groupInput = document.getElementById('group-input');
+    if (!Demo && groupInput.value) {
+        console.log("Ist Nicht DEMO")
+    } else if (Demo) {
+        console.log("Ist DEMO")
+    } else {
+        groupInput.classList.add("red-outline")
+    }
+}
 
 
 
-function hideDiv(){
+function hideDiv(id) {
+    let divId = document.querySelector(id)
+    divId.style.display = "none"
+}
 
-   
-   let groupInput = document.getElementById("group-input")
-   console.log(groupInput)
-    groupInput.classList.add("display-none")
+function showDiv(id) {
+    let divId = document.querySelector(id);
+    divId.style.display = "flex"
+}
+
+function saveArrayInLS(arrayInput) {
+    let arrayAsString = JSON.stringify(arrayInput);
+    localStorage.setItem("allGroups", arrayAsString)
+}
+
+function loadArrayFromLS(arrayInput) {
+    let arrayAsString = localStorage.getItem(arrayInput)
+    let myArray = JSON.parse(arrayAsString)
+    return myArray;
 }
